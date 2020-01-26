d446621490b8fbf89d4607d303836a4b00fe135b
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-01-26 12:35:11 -0300

    aplicando sintax highlighth

---
+++ aulas/receitas/files.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const read = nomeArquivo => {
+  return fs.readFileSync(`${__dirname}/originais/${nomeArquivo}`, {
+    encoding: "utf8"
+  });
+};
+
+const write = (nomeArquivo, conteudo) => {
+  const dirname = `${__dirname}/aterados`;
+
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname);
+  }
+
+  fs.writeFileSync(`${dirname}/${nomeArquivo}`, conteudo, {
+    encoding: "utf8"
+  });
+};
+
+module.exports = { read, write };
